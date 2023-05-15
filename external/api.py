f0e13de28ebeea538ebf3879ab0b640cad78f797
Theo Sun <theo.sun@outlook.com>
2023-05-15 13:02:25 +0800

    feat: open ai chat completion compatible

--- external/api.py
+++ external/api.py
@@ -1,72 +1,64 @@
 import poe
-import envtoml as toml
 import os
 import sys
+import uuid
+import json
+import time
+from werkzeug.exceptions import HTTPException
 from flask import Flask, request, stream_with_context, Response
 from poe import Client
 
-poe.headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 "
-                  "Safari/537.36",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,"
-              "application/signed-exchange;v=b3;q=0.7",
-    "Accept-Encoding": "gzip, deflate, br",
-    "Accept-Language": "zh-CN,zh-TW;q=0.9,zh;q=0.8,en-US;q=0.7,en;q=0.6",
-    "Cache-Control": "no-cache",
-    "Pragma": "no-cache",
-    "Sec-Ch-Ua": "\"Chromium\";v=\"112\", \"Google Chrome\";v=\"112\", \"Not:A-Brand\";v=\"99\"",
-    "Sec-Ch-Ua-Mobile": "?0",
-    "Sec-Ch-Ua-Platform": "\"Windows\"",
-    "Upgrade-Insecure-Requests": "1"
-}
 
-file_path = os.path.abspath(sys.argv[0])
-file_dir = os.path.dirname(file_path)
-config_path = os.path.join(file_dir, "config.toml")
-config = toml.load(config_path)
-proxy = config["proxy"]
-timeout = config["timeout"]
+proxy = os.environ.get("PROXY", None)
+default_bot = os.environ.get("DEFAULT_BOT", "a2")
+timeout = int(os.environ.get("TIMEOUT", '30'), 10)
 app = Flask(__name__)
-client_dict = {}
+
+clients = {}
 
 
 def _get_client(token: str) -> Client:
-  print("Connecting to poe...")
-  client_poe = poe.Client(token, proxy=None if proxy == "" else proxy)
-  return client_poe
+  app.logger.info("Connecting to poe...")
+  return poe.Client(token, proxy=proxy)
 
 
-
-def _register_token(token):
-  if token not in client_dict.keys():
-    try:
-      c = _get_client(token)
-      client_dict[token] = c
-      return "ok"
-    except Exception as exception:
-      print("Failed to connect to poe due to " + str(exception))
-      return "failed: " + str(exception)
+def _register_token(token: str) -> Client:
+  if token not in clients.keys():
+    c = _get_client(token)
+    clients[token] = c
+    app.logger.info(
+        "register client for token xxxx{}xxxx successful".format(token[4:-4]))
+    return c
   else:
-    return "exist"
+    return clients.get(token)
 
 
-def _get_config():
-  bot = 'a2' if request.values['bot'] == None else request.values['bot']
-  token = request.values['token']
-  content = request.values['content']
+def _get_req_config():
+  bot = request.values.get('bot', default_bot)
+  token = request.values.get('token')
+  content = request.values.get("content")
   return token, bot, content
 
-@app.route('/add_token', methods=['GET', 'POST'])
-def add_token():
-  token = request.values['token']
-  return _register_token(token)
+
+@app.route('/', methods=['GET'])
+def index():
+  return {'service': 'poe-api', 'status': 'alive'}
+
+
+@app.route('/register_token', methods=['GET', 'POST'])
+def register_token():
+  token = request.values.get('token')
+  status = 304
+  if token not in clients.keys():
+    status = 201
+    _register_token(token)
+  return Response({'status': 'successful'}, status=status)
 
 
 @app.route('/ask', methods=['GET', 'POST'])
 def ask():
-  token, bot, content = _get_config()
-  _register_token(token)
-  client = client_dict[token]
+  token, bot, content = _get_req_config()
+  client = _register_token(token)
   for chunk in client.send_message(bot, content, with_chat_break=True, timeout=timeout):
     pass
   return Response(chunk["text"].strip(), content_type="text/plain")
@@ -74,16 +66,72 @@ def ask():
 
 @app.route('/ask_stream', methods=['GET', 'POST'])
 def ask_stream():
-  token, bot, content = _get_config()
-  _register_token(token)
-  client = client_dict[token]
+  token, bot, content = _get_req_config()
+  client = _register_token(token)
 
   @stream_with_context
-  def generate():
+  def generate_response():
     for chunk in client.send_message(bot, content, with_chat_break=True, timeout=timeout):
       yield chunk["text_new"]
 
-  return Response(generate(), content_type="text/plain")
+  return Response(generate_response(), content_type="text/plain")
+
+
+@app.post('/chat/completions')
+def chat_completion():
+  token = request.headers.get("authorization").removeprefix("Bearer ")
+  client = _register_token(token)
+  body = request.get_json()
+  messages = body.get('messages')
+  stream = body.get("stream", False)
+  content = "\n".join(
+      map(lambda m: "{}: {}".format(m.get("role"), m.get("content")), messages))
+
+  if stream:
+    def stream():
+      for chunk in client.send_message(default_bot, content, with_chat_break=True, timeout=timeout):
+        yield chunk["text_new"]
+    return Response(stream(), mimetype='text/event-stream')
+
+  else:
+    for chunk in client.send_message(default_bot, content, with_chat_break=True, timeout=timeout):
+      pass
+    return {
+        "id": uuid.uuid4(),
+        "object": "chat.completion",
+        "created": int(time.time_ns()),
+        "model": default_bot,
+        "choices": [
+            {
+                "index": 0,
+                "message": {
+                    "role": "assistant",
+                    "content": chunk['text'].strip()
+                },
+                "finish_reason": "stop"
+            }
+        ],
+        "usage": {
+            "prompt_tokens": 0,
+            "completion_tokens": 0,
+            "total_tokens": 0
+        }
+    }
+
+
+@app.errorhandler(HTTPException)
+def handle_exception(e):
+  """Return JSON instead of HTML for HTTP errors."""
+  # start with the correct headers and status code from the error
+  response = e.get_response()
+  # replace the body with JSON
+  response.data = json.dumps({
+      "code": e.code,
+      "name": e.name,
+      "description": e.description,
+  })
+  response.content_type = "application/json"
+  return response
 
 
 if __name__ == '__main__':
